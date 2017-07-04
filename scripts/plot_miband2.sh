c7eba0b54e21071dcfd422434212886ce8aab0f0
Joe Desbonnet <jdesbonnet@gmail.com>
2017-07-04 22:18:40 +0100

    allow for more than one device/user

--- scripts/plot_miband2.sh
+++ scripts/plot_miband2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 DB=$1
-NREC=20000
+NREC=1000
 
-./read_gadgetbridge_db.sh $DB > t.dat
-tail -n ${NREC} t.dat > a.dat
-mv a.dat t.dat
+./read_gadgetbridge_db.sh $DB 2 > miband2.dat
+tail -n ${NREC} miband2.dat > t.dat
+
 gnuplot <<EOF
 
-set title "Sleeping heart rate data from Mi Band 2"
+set title "Activity and sleeping heart rate data from Mi Band 2"
 
 set terminal pngcairo size 1400,600
 set output "sleep.png"
@@ -16,9 +16,9 @@ set output "sleep.png"
 set grid
 set xdata time
 set timefmt "%s"
-set format x "%H:%M"
+set format x "%H:%M\n%a"
 set yrange [0:120]
 set ylabel "Beats per minute (bpm)"
 
-plot 't.dat' using 1:7 title "Sleeping heart rate (bpm)" , '' using 1:4 with impulses lt 3
+plot 't.dat' using 1:7 title "Sleeping heart rate (bpm)" , '' using 1:4 with impulses lt 3 title 'Activity'
 EOF
