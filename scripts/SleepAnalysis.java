3c8af6bbb002622849558165f62208e3304393c5
Joe Desbonnet <jdesbonnet@gmail.com>
2017-07-31 21:43:55 +0100

    add heart rate mean, sd etc

--- scripts/SleepAnalysis.java
+++ scripts/SleepAnalysis.java
@@ -21,9 +21,18 @@ public class SleepAnalysis {
 
 		String line,date,lastDate="";
 		int activity,mainSleep=0,napSleep=0, hour;
+		int hr;
+		int sigma_hr = 0;
+		int sigma_hr2 = 0;
+		int hr_min = 999;
+		int hr_max = 0;
+		int hr_count = 0;
 		long time;
 
 		LineNumberReader r = new LineNumberReader(new FileReader(arg[0]));
+
+
+		System.out.println ("# date main_sleep_hours nap_hours hr_mean hr_sd hr_min hr_max");
 
 		while ( (line = r.readLine()) != null) {
 		
@@ -36,19 +45,41 @@ public class SleepAnalysis {
 			if (!date.equals(lastDate)) {
 				double mainSleepHours = (double)mainSleep / 60.0;
 				double napSleepHours = (double)napSleep / 60.0;
+				double n = (double)hr_count;
+				double hr_mean = (double)sigma_hr / n;
+				double hr_sd = Math.sqrt( (sigma_hr2 - (sigma_hr*sigma_hr)/n) / (n-1) );
 				System.out.println (lastDate 
 					+ " " + String.format("%.1f",mainSleepHours)
 					+ " " + String.format("%.1f",napSleepHours)
+					+ " " + String.format("%.1f",hr_mean)
+					+ " " + String.format("%.2f",hr_sd)
+					+ " " + hr_min
+					+ " " + hr_max
 				);
-				mainSleep = 0;
-				napSleep = 0;
+				mainSleep = napSleep = 0;
+				sigma_hr = sigma_hr2 = 0;
+				hr_count = 0;
+				hr_min = 999;
+				hr_max = 0;
 			}
 
 			activity = Integer.parseInt(p[5]);
+			hr = Integer.parseInt(p[6]);
 
 			if (activity == SLEEP) {
 				if (hour < 12) {
 					mainSleep++;
+					if (hr != 255) {
+						sigma_hr += hr;
+						sigma_hr2 += hr*hr;
+						if (hr > hr_max) {
+							hr_max = hr;
+						}
+						if (hr < hr_min) {
+							hr_min = hr;
+						}
+						hr_count++;
+					}
 				} else {
 					napSleep++;
 				}
